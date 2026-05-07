df8446c24c94312139c8a545b95fd37ca47aa934
Rasel6268 <bmrasel626825@gmail.com>
2026-05-07 18:01:16 +0600

    problem: getDetails

--- solutions.ts
+++ solutions.ts
@@ -45,3 +45,47 @@ const toggleReadStatus = (book: Book): readBook  => {
     isRead: true,
   };
 };
+//problem6
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string,age: number){
+        this.name = name;
+        this.age = age
+    };
+    stdInfo(): string {
+      return `Name: ${this.name},age: ${this.age}`
+    }
+}
+class Student extends Person {
+     grade: string;
+    constructor(name: string,age:number,grade:string){
+    
+    super(name,age);
+    this.grade = grade
+    }
+    getDetails():string {
+        return `${this.stdInfo()},Grade: ${this.grade}`
+    }
+}
+
+
+// class Person {
+//   constructor(public name: string) {}
+
+//   greet(): void {
+//     console.log(`Hello, ${this.name}`);
+//   }
+// }
+
+// class Student extends Person {
+//   constructor(name: string, public roll: number) {
+//     super(name);
+//   }
+// }
+
+// const student1 = new Student("Rasel", 101);
+
+// student1.greet();
+// console.log(student1.roll);
