9d39c545721e0f907510886026c62894ebe46b83
Rasel6268 <bmrasel626825@gmail.com>
2026-05-07 17:20:44 +0600

    problem: toggleReadStatus

--- solutions.ts
+++ solutions.ts
@@ -1,7 +1,7 @@
 //Problem 1:
 
 const filterEvenNumbers = (num: number[]): number[] => {
-  let evenNumber: number[] = []
+  let evenNumber: number[] = [];
   for (let i = 0; i < num.length; i++) {
     if (num[i] % 2 === 0) {
       evenNumber.push(num[i]);
@@ -11,15 +11,37 @@ const filterEvenNumbers = (num: number[]): number[] => {
 };
 
 //Problem2
-const reverseString =(str:string): String =>{
-    let reverseStr: string = ''
-    for(let i = str.length - 1 ;i >= 0;i--){
-       reverseStr += str[i]
-    }
-  return reverseStr
+const reverseString = (str: string): String => {
+  let reverseStr: string = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+};
+//problem3
+const checkType = (type: String | number): string => {
+  return typeof type;
+};
+
+//problem4
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key];
+};
+//problem5
+
+interface Book {
+  title: String;
+  author: String;
+  publishedYear: number;
 }
 
-const checkType = (type: String |number) : string => {
-      return typeof type
+interface readBook extends Book {
+    isRead: boolean
 }
 
+const toggleReadStatus = (book: Book): readBook  => {
+  return {
+    ...book,
+    isRead: true,
+  };
+};
